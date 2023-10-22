1bf9733c2c5865e25adfbcc6f43bcdbac0e88dc0
nigel <nigelchimwene@gmail.com>
2023-10-22 17:28:54 +0300

    Initial Commit

---
+++ UItest/UItestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UItestApp.swift
+//  UItest
+//
+//  Created by nigel chimwene on 22/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct UItestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
