c45c77392c059c95147aedba4b3c46413482b885
nigel <nigelchimwene@gmail.com>
2023-10-22 18:08:56 +0300

    padding leading

--- UItest/ContentView.swift
+++ UItest/ContentView.swift
@@ -9,13 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Nigel Beast")
-        }
-        .padding()
+        Text("Nigel beast").padding(20)
+            .background(Color.blue)
     }
 }
 
