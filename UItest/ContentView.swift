c63301176d9a58646b0b39a2b80803b9eadf8343
nigel <nigelchimwene@gmail.com>
2023-10-23 11:24:48 +0300

    logo

--- UItest/ContentView.swift
+++ UItest/ContentView.swift
@@ -9,10 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Nigel beast")
-            .multilineTextAlignment(.trailing)
-            .padding(20)
-            .background(Color.blue)
+        
+        VStack{  //accepts only upto 10 stacks
+            Image("logo").resizable().aspectRatio(contentMode: .fit)
+            
+            Text("Unleash the dogs of war")
+        }
+       
     }
 }
 
